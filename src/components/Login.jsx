d04d96f5f19a18e0c3d9d43ef55f50d00143ff53
Mathias Curbelo <104522465+na7hk3r@users.noreply.github.com>
2024-10-23 23:24:41 -0300

    (add&fix) Corrección de algunos estilos e implementación de variables de la paleta de colores propuesta. (fix) H1 de Buscador ahora es visible

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,8 +4,8 @@ import Button from './Button'
 const Login = () => {
   return (
     <div className='flex gap-5 px-5'>
-        <Button text='Iniciar sesión' bgColor="black" textColor="amber-400"/>
-        <Button text='Registrarse' bgColor="amber-400" textColor="black"/>
+        <Button text='Iniciar sesión' bgColor="black" textColor="primary"/>
+        <Button text='Registrarse' bgColor="amber-400" textColor="primary"/>
     </div>
   )
 }
