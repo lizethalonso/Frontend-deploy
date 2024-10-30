fe6bf7acde03672dc4d6584f51d69a69da0597e8
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-10-30 20:48:43 -0300

    fix create product form

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,15 @@ const Header = () => {
 	const { isMobile } = useContextGlobal(); 
 
 	const buttons = {
-		1: [{ text: "Iniciar sesión", bgColor: "primary", textColor: "black", textSize:"sm" ,action: "" }],
-		2: [{ text: "Registrarse", bgColor: "primary", textColor: "black", textSize:"sm" ,action: "" }],
+		1: [{ text: "Iniciar sesión", bgColor: "primary", textColor: "black", textSize:"sm", action: () => console.log('Iniciar sesión') }],
+		2: [{ text: "Registrarse", bgColor: "primary", textColor: "black", textSize:"sm", action: () => console.log('Registrarse') }],
 	};
+	
 
 	return (
 		<header className="flex bg-background justify-between items-center fixed top-0 w-screen h-24 z-10 p-4  border-primary/50 ">
 			
-			<Logo size={20} />
+			<Logo size={16} />
 			<ButtonSet buttons={buttons} />
 		</header>
 	);
