0daf3bc092809806e0330327b29c02ac702fbd2c
Maria Elena Haydee Ponce <120025349+hechizera10@users.noreply.github.com>
2024-10-31 21:16:37 -0300

    install cloudinary

---
+++ src/utils/cloudinaryConfig.js
@@ -0,0 +1,7 @@
+import {Cloudinary} from "@cloudinary/url-gen";
+// Create a Cloudinary instance and set your cloud name.
+const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dr1jbzn9r'
+    }
+  });
